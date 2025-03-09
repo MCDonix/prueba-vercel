9a20ea1e9f0b11982fdc6ff05f07051fef98bc71
unknown <mauricio.gonzalest@gmail.com>
2025-03-09 09:19:41 -0300

    prueba rutas browser

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "../Pages/Home/Home"
 import { Contact } from "../Pages/Contact/Contact"
 import { Apidata } from "../Pages/Apidata/Apidata";
@@ -6,14 +6,14 @@ import { Apidata } from "../Pages/Apidata/Apidata";
 
 const Router = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/api-data" element={<Apidata />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
 
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
